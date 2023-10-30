d8357c2cd65f7dd09b608c33ead6e19545c3f8eb
atomgx7 <atomgx7@gmail.com>
2023-10-29 19:31:08 -0600

    Corregido error al cargar las variables del .env

--- GlobalDeployCommands.js
+++ GlobalDeployCommands.js
@@ -34,7 +34,7 @@ const rest = new REST().setToken(process.env.TOKEN);
 
 		// The put method is used to fully refresh all commands in the guild with the current set
 		const data = await rest.put(
-            Routes.applicationCommands(CLIENT_ID),
+            Routes.applicationCommands(process.env.CLIENT_ID),
             { body: commands },
         );
 
